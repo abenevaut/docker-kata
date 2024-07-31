e1ce974519b9c0b5fce63f31626d1a4d66bb65cc
Antoine B. <me@abenevaut.dev>
2024-07-31 19:54:51 +0200

    feat: build your .NET app with docker compose

---
+++ 008-create-web-api-docker-compose/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _004_web_api_docker_composer
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
